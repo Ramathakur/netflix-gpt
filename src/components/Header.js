0aaf3186e2422c2f2d6af9437cb9ce382bbefcbc
rama.t <rama.t@cisinlabs.com>
2024-05-14 19:16:43 +0530

    setup firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { LOGO } from '../utils/constant'
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  // const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => { })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    // Unsiubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
-    <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
     </div>
   )
 }
