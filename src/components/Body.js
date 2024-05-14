817adbaf1288bb2a5a212a9716be914a5e9b36c5
rama.t <rama.t@cisinlabs.com>
2024-05-14 19:27:04 +0530

    create complete netflix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import Login from './Login'
-import Browse from './Browse'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
+import Browse from "./Browse";
+import Login from "./Login";
+import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
-    const appRouter = createBrowserRouter([
-        {
-        path: '/', 
-        element: <Login />,
-    },{
-        path: '/browse', 
-        element: <Browse />
-    }
-])
-  return (
-    <>
-    <RouterProvider router={appRouter} />
-    </>
-  )
-}
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
-export default Body
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+export default Body;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,22 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBgT4VmkqauxrysI_lyCQxCMCubCqSswj4",
-    authDomain: "netflixgpt-71e17.firebaseapp.com",
-    projectId: "netflixgpt-71e17",
-    storageBucket: "netflixgpt-71e17.appspot.com",
-    messagingSenderId: "997052511342",
-    appId: "1:997052511342:web:6060a9baae1323e40fe27f",
-    measurementId: "G-N3KP02X6VF"
+  apiKey: "AIzaSyBgT4VmkqauxrysI_lyCQxCMCubCqSswj4",
+  authDomain: "netflixgpt-71e17.firebaseapp.com",
+  projectId: "netflixgpt-71e17",
+  storageBucket: "netflixgpt-71e17.appspot.com",
+  messagingSenderId: "997052511342",
+  appId: "1:997052511342:web:6060a9baae1323e40fe27f",
+  measurementId: "G-N3KP02X6VF"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 export const auth = getAuth();
